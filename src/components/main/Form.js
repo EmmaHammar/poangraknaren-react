444c0bf913c52bbc7d05686d712b6a979f155f06
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-06-14 23:55:26 +0200

    adds onChange() to Form

--- src/components/main/Form.js
+++ src/components/main/Form.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 
 class Form extends Component {
+
+    state = {
+        inputPlayer:this.props.inputPlayer,
+        inputScore:this.props.inputScore,
+    }
+
+    onChange = (evt) => {
+        console.log("upptäckte en ändring");
+    }
+
     render() {
         return(
             <form>
-                <input type="text" id="inputPlayer" placeholder="Player" />
-                <input type="number" id="inputScore" placeholder="Score" />
+                <input type="text" id="inputPlayer" placeholder="Player" value={this.state.inputPlayer} onChange={this.onChange} />
+                <input type="number" id="inputScore" placeholder="Score" value={this.state.inputScore} onChange={this.onChange}/>
                 <button id="addPlayerBtn">Add Player</button>
             </form>
         );
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -2,10 +2,21 @@ import React, { Component } from "react";
 import Form from "./Form";
 
 class Main extends Component {
+
+    state = {
+        inputPlayer: "",
+        inputScore: "",
+        // inputScore: "skriv score", //varför syns inte detta medan om jag skriver inputPlayer: "skriv player", syns det?
+    }
+
     render() {
         return(
         <>
-            <Form />
+            <Form 
+                inputPlayer={this.state.inputPlayer}
+
+                inputScore={this.state.inputScore}
+            />
         </>
         );
     };
