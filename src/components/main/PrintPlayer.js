02559425e6efd41b620b869e7383c1c6586fae16
Emma Hammar <emmahammar@emmas-macbook-2.lan>
2021-06-15 13:52:31 +0200

    onclick + and - buttons

--- src/components/main/PrintPlayer.js
+++ src/components/main/PrintPlayer.js
@@ -3,14 +3,22 @@ import React from "react";
 
 class PrintPlayer extends React.Component {
 
+    addScore = (evt) => {
+        console.log("add score");
+    }
+
+    removeScore = (evt) => {
+        console.log("remove score");
+    }
+
     render() {
         return (
             <div className="playerInfoDiv">
 
                 <h2>{this.props.pName}</h2>
                 <h3>{this.props.score} poäng</h3>
-                <button id="minus-btn" className="btn">-</button>
-                <button id="plus-btn" className="btn">+</button>
+                <button onClick={this.removeScore} id="minus-btn" className="btn">-</button>
+                <button onClick={this.addScore} id="plus-btn" className="btn">+</button>
 
             </div>
         )
